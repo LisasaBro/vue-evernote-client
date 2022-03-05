dd0ee987629fde8a3876aad75bd68de5211ee58e
sasaLi <850852006@qq.com>
2022-03-05 19:14:05 +0800

    登陆、注册、注销后跳转页面

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <Siderbar/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+import Siderbar from './components/Sidebar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Siderbar
+  }
+}
+</script>
+
+<style>
+@import '//at.alicdn.com/t/font_496303_kqrjhri8l25d0a4i.css';
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+html, body, #app {
+  height: 100%;
+}
+
+body {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: #eee;
+
+}
+a{
+  text-decoration: none;
+  color: #444;
+}
+ul,li{
+  list-style: none;
+}
+.bth{
+  color: #666;
+  font-size: 12px;
+  padding: 2px 4px;
+  background: #fff;
+  box-shadow: 0 0 2px 0 #ccc;
+  border: none;
+  cursor: pointer;
+  display: inline-block;
+}
+#app{
+  display: flex;
+  align-items: stretch;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '../components/Login'
+import notebookList from '../components/NotebookList'
+import noteDetail from '../components/NoteDetail'
+import TrashDetail from '../components/TrashDetail'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: Login
+    },
+    {
+      path: '/notebooks',
+      component: notebookList
+    },
+    {
+      path: '/note/:noteId',
+      component: noteDetail
+    },
+    {
+      path:'/trash/:noteId',
+      component:TrashDetail
+    }
+  ]
+})
