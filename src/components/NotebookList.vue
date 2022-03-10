9839edcbab5a520ef0614fc99a07a95ca423cf20
sasaLi <850852006@qq.com>
2022-03-11 01:35:42 +0800

    使用vuex重构NoteDetail

--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -5,15 +5,15 @@
     </header>
     <main>
       <div class="layout">
-        <h3>笔记本列表({{ notebooks.length }})</h3>
+        <h3>笔记本列表({{notebooks.length}})</h3>
         <div class="book-list">
           <router-link v-for="(notebook,index) in notebooks" :key="index" :to="`/note?notebookId=${notebook.id}`" class="notebook">
             <div>
-              <span class="iconfont icon-notebook"></span> {{ notebook.title }}
-              <span>{{ notebook.noteCounts }}</span>
+              <span class="iconfont icon-notebook"></span> {{notebook.title}}
+              <span>{{notebook.noteCounts}}</span>
               <span class="action" @click.stop.prevent="onEdit(notebook)">编辑</span>
               <span class="action" @click.stop.prevent="onDelete(notebook)">删除</span>
-              <span class="date">{{ notebook.friendlyCreatedAt }}</span>
+              <span class="date">{{notebook.friendlyCreatedAt}}</span>
             </div>
           </router-link>
         </div>
@@ -26,76 +26,65 @@
 <script>
 import Auth from '@/apis/auth'
 import Notebooks from '@/apis/notebooks'
-import {friendlyDate} from '@/helpers/util'
+import { friendlyDate } from '@/helpers/util'
+import { mapState, mapActions, mapGetters } from 'vuex'
 
 //window.Notebooks = Notebooks
 
 export default {
-  data() {
-    return {
-      notebooks: []
-    }
+  data () {
+    return {}
   },
 
   created() {
-    Auth.getInfo()
-      .then(res => {
-        if (!res.isLogin) {
-          this.$router.push({path: '/login'})
-        }
-      })
+    this.checkLogin({ path: '/login' })
+    this.getNotebooks()
+  },
 
-    Notebooks.getAll()
-      .then(res => {
-        this.notebooks = res.data
-      })
+  computed: {
+    ...mapGetters(['notebooks'])
   },
 
   methods: {
+    ...mapActions([
+      'getNotebooks',
+      'addNotebook',
+      'updateNotebook',
+      'deleteNotebook',
+      'checkLogin'
+    ]),
+
     onCreate() {
-      this.$prompt('请输入新的笔记本标题', '创建笔记本', {
+      this.$prompt('输入新笔记本标题', '创建笔记本', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
-        inputPattern: /^.{1,35}$/,
-        inputErrorMessage: '标题不得为空且不得大于30个字符！'
-      }).then(({value}) => {
-        return Notebooks.addNoteBook({title:value})
-
-      }).then(res => {
-        res.data.friendlyCreatedAt = friendlyDate(res.data.createdAt)
-        this.notebooks.unshift(res.data)
-        this.$message.success(res.msg)
+        inputPattern: /^.{1,30}$/,
+        inputErrorMessage: '标题不能为空，且不超过30个字符'
+      }).then(({ value }) => {
+        this.addNotebook({ title: value })
       })
-
     },
 
     onEdit(notebook) {
       let title = ''
-      this.$prompt('请输入新的笔记本标题', '修改笔记本', {
+      this.$prompt('输入新笔记本标题', '修改笔记本', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
-        inputPattern: /^.{1,35}$/,
-        inputValue:notebook.title,
-        inputErrorMessage: '标题不得为空且不得大于30个字符！'
-      }).then(({value}) => {
-        title = value
-        return Notebooks.updateNotebook(notebook.id, {title})
-      }).then(res => {
-        notebook.title = title
-        this.$message.success(res.msg)
+        inputPattern: /^.{1,30}$/,
+        inputValue: notebook.title,
+        inputErrorMessage: '标题不能为空，且不超过30个字符'
+      }).then(({ value }) => {
+        this.updateNotebook({ notebookId: notebook.id, title: value })
       })
     },
 
     onDelete(notebook) {
-      this.$confirm('此操作将删除该笔记, 是否继续?', '提示', {
+      this.$confirm('确认要删除笔记本吗', '删除笔记本', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
-      }).then(() =>{
-        return Notebooks.deleteNotebook(notebook.id)
-      }).then(res =>{
-        this.notebooks.splice(this.notebooks.indexOf(notebook), 1)
-        this.$message.success(res.msg)
+      }).then(() => {
+        this.deleteNotebook({ notebookId: notebook.id })
       })
     }
   }
